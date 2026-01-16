7268422ea672c65c3114533e59faed5f88d23834
Denis Bazhenov <dotsid@gmail.com>
2026-01-16 19:05:38 +0100

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,45 @@
+use agent_client_protocol::{self as acp, RawValue};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 #[cfg_attr(test, derive(Debug, PartialEq))]
-struct NewSessionMeta {
+pub struct NewSessionMeta {
     #[serde(rename = "remote")]
-    remote: NewSessionRemote,
+    pub remote: NewSessionRemote,
 
     #[serde(rename = "jbAiToken")]
-    jb_ai_token: String,
+    pub jb_ai_token: String,
 
     #[serde(rename = "supportsUserGitAuthFlow")]
-    supports_user_git_auth_flow: bool,
+    pub supports_user_git_auth_flow: bool,
 }
 
 #[derive(Serialize, Deserialize)]
 #[cfg_attr(test, derive(Debug, PartialEq))]
-struct NewSessionRemote {
+pub struct NewSessionRemote {
     #[serde(rename = "branch")]
-    branch: String,
+    pub branch: String,
 
     #[serde(rename = "url")]
-    url: String,
+    pub url: String,
 
     #[serde(rename = "revision")]
-    revision: Option<String>,
+    pub revision: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RawIncomingMessage<'a> {
+    pub id: Option<acp::RequestId>,
+    pub method: Option<&'a str>,
+    pub params: Option<&'a RawValue>,
+    pub result: Option<&'a RawValue>,
+    pub error: Option<acp::Error>,
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use agent_client_protocol::{AgentSide, ClientRequest, Side};
     use serde::de::DeserializeOwned;
     use serde_json::Value;
     use std::fmt::Debug;
@@ -49,12 +60,41 @@ mod tests {
                 remote: NewSessionRemote {
                     branch: "main".to_string(),
                     url: "https://example.com/repo.git".to_string(),
-                    revision: Some("18adf27d36912b2e255c71327146ac21116e232f".to_string()),
+                    revision: "18adf27d36912b2e255c71327146ac21116e232f".to_string(),
                 },
                 jb_ai_token: "test_token".to_string(),
                 supports_user_git_auth_flow: false,
             },
         );
+    }
+
+    #[test]
+    fn json_rpc_request_can_be_deserialized_using_raw_request() {
+        let json = r#"{
+            "jsonrpc":"2.0",
+            "id":0,
+            "method":"initialize",
+            "params": {
+                "protocolVersion":1,
+                "clientCapabilities": {
+                    "fs": {
+                        "readTextFile":true,
+                        "writeTextFile":true
+                    },
+                    "terminal":true,
+                    "_meta": {},
+                    "clientInfo": {"name":"ide","title":"IDE","version":"0.1"}
+                }
+            }
+        }"#;
+
+        let raw_message: RawIncomingMessage = serde_json::from_str(json).unwrap();
+        let request =
+            AgentSide::decode_request(raw_message.method.unwrap(), raw_message.params).unwrap();
+
+        if !matches!(request, ClientRequest::InitializeRequest(..)) {
+            panic!("Unexpected request: {:?}", request);
+        }
     }
 
     fn check_serialization<T>(json: &str, expected_value: T)
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-use agent_client_protocol as acp;
+use acp_jcp::{NewSessionMeta, NewSessionRemote, RawIncomingMessage};
+use agent_client_protocol::{self as acp, ClientRequest, IncomingMessage, Side};
 use agent_client_protocol::{
-    AgentSide, ClientCapabilities, ClientSide, FileSystemCapability, InitializeRequest,
-    JsonRpcMessage, OutgoingMessage, ProtocolVersion, Request, RequestId,
+    AgentSide, ClientCapabilities, ClientSide, FileSystemCapability, JsonRpcMessage,
+    OutgoingMessage, ProtocolVersion, Request, RequestId,
 };
 use dotenv::dotenv;
-use futures_util::{FutureExt, SinkExt, StreamExt};
-use std::env;
-use tokio::io::{AsyncBufReadExt, BufReader, stdin, stdout};
+use futures_util::{SinkExt, StreamExt};
+use serde_json::{Map, Value};
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::Write;
+use std::{env, io};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, stdin, stdout};
 use tokio_tungstenite::connect_async;
 use tungstenite::{Message, Utf8Bytes, client::IntoClientRequest};
 
@@ -16,56 +21,133 @@ async fn main() {
 
     let jba_access_token =
         env::var("JBA_ACCESS_TOKEN").expect("JBA_ACCESS_TOKEN env variable should be configured");
+    let git_url = env::var("GIT_URL").expect("GIT_URL env variable should be configured");
 
-    let client_tx = stdin();
-    let client_rx = stdout();
+    let antropic_key =
+        env::var("ANTHROPIC_KEY").expect("ANTHROPIC_KEY env variable should be configured");
 
-    let mut request = "wss://api.stgn.jetbrainscloud.com/agent-spawner/acp"
-        .into_client_request()
-        .unwrap();
+    let jcp_url = env::var("JCP_URL")
+        .ok()
+        .unwrap_or("wss://api.stgn.jetbrainscloud.com/agent-spawner/acp".into());
+
+    let mut request = jcp_url.into_client_request().unwrap();
     request.headers_mut().insert(
         "Authorization",
         format!("Bearer {jba_access_token}").parse().unwrap(),
     );
 
+    let log_file = env::var("TRAFFIC_LOG")
+        .ok()
+        .and_then(|f| File::create(f).ok());
+    let mut traffic_log = TrafficLog(log_file);
+
     let (ws_stream, _) = connect_async(request).await.unwrap();
+    let (mut server_tx, mut server_rx) = ws_stream.split();
 
-    let (mut write, mut read) = ws_stream.split();
+    let mut client_rx = stdout();
+    let client_tx = BufReader::new(stdin());
+    let mut client_tx_lines = client_tx.lines();
 
-    let a = read.next().await.unwrap().unwrap();
+    loop {
+        tokio::select! {
+            client_to_server_msg = client_tx_lines.next_line() => {
+                if let Some(msg) = client_to_server_msg.unwrap() {
+                    let rpc_msg: RawIncomingMessage<'_> =
+                        serde_json::from_slice(msg.as_bytes()).unwrap();
 
-    let r = BufReader::new(client_tx);
-    let mut lines = r.lines();
+                    if let Some((method, id)) = rpc_msg.method.zip(rpc_msg.id) {
+                        let mut request = AgentSide::decode_request(method, rpc_msg.params).unwrap();
 
-    tokio::select! {
-        client_to_server_msg = lines.next_line() => {
+                        if let ClientRequest::NewSessionRequest(mut r) = request {
+                            // Inject session/new meta
+                            let remote_meta = NewSessionMeta {
+                                remote: NewSessionRemote {
+                                    branch: "main".into(),
+                                    url: git_url.clone(),
+                                    revision: "main".into(),
+                                },
+                                jb_ai_token: antropic_key.clone(),
+                                supports_user_git_auth_flow: false
+                            };
 
+                            let Value::Object(meta) = serde_json::to_value(remote_meta).unwrap() else {
+                                panic!("Unexpected value");
+                            };
+                            r = r.meta(meta);
+                            request = ClientRequest::NewSessionRequest(r);
+                        }
+
+                        let msg = JsonRpcMessage::wrap(OutgoingMessage::Request::<ClientSide, AgentSide>(Request {
+                            id,
+                            method: method.into(),
+                            params: Some(request)
+                        }));
+
+                        let json = serde_json::to_string(&msg).unwrap();
+                        traffic_log.log(&json);
+
+                        server_tx
+                            .send(Message::Text(Utf8Bytes::from(&json)))
+                            .await
+                            .unwrap();
+                    } else {
+                        // Sending notifications to an JCP without modification
+                        traffic_log.log(&msg);
+                        server_tx
+                            .send(Message::Text(Utf8Bytes::from(msg)))
+                            .await
+                            .unwrap();
+                    }
+                } else {
+                    break;
+                }
+            },
+
+            server_to_client_msg = server_rx.next() => {
+                if let Some(msg) = server_to_client_msg.transpose().unwrap() {
+                    let data = msg.into_data();
+                    traffic_log.log(&data);
+                    client_rx.write_all(&data).await.unwrap();
+                    // We need to add newline to frame an JSON-RPC message in stdout
+                    client_rx.write_all(b"\n").await.unwrap();
+                } else {
+                    break;
+                }
+            }
         }
     }
 
-    let rq = acp::ClientRequest::InitializeRequest(
-        acp::InitializeRequest::new(ProtocolVersion::V1).client_capabilities(
-            ClientCapabilities::new()
-                .terminal(false)
-                .fs(FileSystemCapability::new()
-                    .read_text_file(false)
-                    .write_text_file(false)),
-        ),
-    );
+    // let rq = acp::ClientRequest::InitializeRequest(
+    //     acp::InitializeRequest::new(ProtocolVersion::V1).client_capabilities(
+    //         ClientCapabilities::new()
+    //             .terminal(false)
+    //             .fs(FileSystemCapability::new()
+    //                 .read_text_file(false)
+    //                 .write_text_file(false)),
+    //     ),
+    // );
 
-    let rpc = JsonRpcMessage::wrap(OutgoingMessage::Request::<ClientSide, AgentSide>(Request {
-        id: RequestId::Number(1),
-        method: "initialize".into(),
-        params: Some(rq),
-    }));
+    // let rpc = JsonRpcMessage::wrap(OutgoingMessage::Request::<ClientSide, AgentSide>(Request {
+    //     id: RequestId::Number(1),
+    //     method: "initialize".into(),
+    //     params: Some(rq),
+    // }));
 
-    let string = serde_json::to_string(&rpc).unwrap();
-    write
-        .send(Message::Text(Utf8Bytes::from(&string)))
-        .await
-        .unwrap();
+    // let string = serde_json::to_string(&rpc).unwrap();
 
-    let reply = read.next().await.unwrap().unwrap();
+    // let reply = server_rx.next().await.unwrap().unwrap();
 
-    println!("{reply}");
+    // println!("{reply}");
+}
+
+/// Simple wrapper that writes a copy of all traffic in a log file
+struct TrafficLog(Option<File>);
+
+impl TrafficLog {
+    fn log(&mut self, data: impl AsRef<[u8]>) {
+        if let Some(file) = self.0.as_mut() {
+            let _ = file.write_all(data.as_ref());
+            let _ = file.write_all(b"\n");
+        }
+    }
 }
